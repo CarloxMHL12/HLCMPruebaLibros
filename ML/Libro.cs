32dc48ebea8637b0a33806bef57df3f192047647
CarloxMHL12 <carlosmiguelhl@outlook.com>
2026-01-15 15:36:57 -0600

    Se creo la rama API-REST, los ML de Autor, Editorial y Libro con sus respectivas Entidades.

---
+++ ML/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Libro
+    {
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public int AnioPublicacion { get; set; }
+        public List<object> Libros { get; set; }
+        public ML.Autor Autor { get; set; }
+        public ML.Editorial Editorial { get; set; }
+    }
+}
