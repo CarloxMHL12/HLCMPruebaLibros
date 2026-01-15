fefe7cde9f3f3e73608a5130e397419d76e21066
CarloxMHL12 <carlosmiguelhl@outlook.com>
2026-01-15 16:57:56 -0600

    Se creo la clase "Libro" para ingresar los metodos, en este caso se realizo el Metodo "GetAll", se probo en POSTMAN y funciona correctamente.

---
+++ BL/Libro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Libro
+    {
+
+
+        public static ML.Result GetAll(ML.Libro libroBusqueda)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.HLCMPruebaLibrosEntities context = new DL.HLCMPruebaLibrosEntities())
+                {
+                    var registros = context.LibroGetAll(libroBusqueda.Autor.Nombre, libroBusqueda.Titulo, libroBusqueda.AnioPublicacion, libroBusqueda.Editorial.Nombre).ToList();
+
+                    if (registros != null)
+                    {
+                        result.Objects = new List<object>();
+
+                        foreach (var libros in registros)
+                        {
+                            ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = libros.IdLibro;
+                            libro.Titulo = libros.Titulo;
+                            libro.AnioPublicacion = libros.AnioPublicacion;
+
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.Nombre = libros.NombreAutor;
+                            libro.Autor.Apellidos = libros.Apellidos;
+
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.Nombre = libros.NombreEditorial;
+
+                            result.Objects.Add(libro);
+                        }
+
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No hay Información que mostrar";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ SL_API/Controllers/LibroController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+
+
+namespace SL_API.Controllers
+{
+    [RoutePrefix("api/Libro")]
+    public class LibroController : ApiController
+    {
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            ML.Libro libro = new ML.Libro();
+            libro.Autor = new ML.Autor();
+            libro.Editorial = new ML.Editorial();
+            libro.Autor.Nombre = "";
+            libro.Titulo = "";
+            libro.AnioPublicacion = 0;
+            libro.Editorial.Nombre = "";
+
+            ML.Result result = BL.Libro.GetAll(libro);
+
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+
+        }
+
+
+
+
+    }
+}
