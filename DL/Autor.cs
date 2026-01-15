622a4611c71a93676afbf30df8eeaede429ac52b
CarloxMHL12 <carlosmiguelhl@outlook.com>
2026-01-15 15:26:06 -0600

    Se tuvo qe Actualizar el Modelo de EntityFramework ya que se hicieron cambios en los SP, Ya funcionan bien.

--- DL/Autor.cs
+++ DL/Autor.cs
@@ -22,6 +22,7 @@ namespace DL
     
         public int IdAutor { get; set; }
         public string Nombre { get; set; }
+        public string Apellidos { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Libro> Libroes { get; set; }
